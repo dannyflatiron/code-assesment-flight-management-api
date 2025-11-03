48aee994b6dcf595e67a3031335ad3a03d465b2d
Danny Reina <dannyflatiron@gmail.com>
2025-11-02 21:54:03 -0500

    chore: data shape required when creating a new flight

--- backend-nestjs/src/flights/flight.types.ts
+++ backend-nestjs/src/flights/flight.types.ts
@@ -3,6 +3,11 @@ export type Flight = {
   flight_number: string;
   origin: string;
   destination: string;
-  departure_time: Date;
-  arrival_time: Date;
+  departure_time: string;
+  arrival_time: string;
 };
+
+export type CreateFlightInput = Pick<
+  Flight,
+  'flight_number' | 'origin' | 'destination' | 'departure_time' | 'arrival_time'
+>;
