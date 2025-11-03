94e5b636e0603b0c759810215bfd197197bc140a
Danny Reina <dannyflatiron@gmail.com>
2025-11-02 19:13:39 -0500

    setup validations

--- backend-nestjs/src/app.module.ts
+++ backend-nestjs/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DbModule } from './db/db.module';
+import { FlightsModule } from './flights/flights.module';
+import { HealthController } from './health.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DbModule, FlightsModule],
+  controllers: [HealthController],
 })
 export class AppModule {}
