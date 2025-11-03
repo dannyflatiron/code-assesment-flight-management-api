666ed0ff57cc9fa0ce20d601ebc7fde765a2dba8
Danny Reina <dannyflatiron@gmail.com>
2025-11-02 21:58:11 -0500

    fix: syntax error

--- backend-nestjs/src/flights/flights.module.ts
+++ backend-nestjs/src/flights/flights.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { FlightsController } from './flights.controller';
 import { FlightsService } from './flights.service';
 import { DbModule } from '../db/db.module';
+import { Pool } from 'pg';
 
 @Module({
   imports: [DbModule],
   controllers: [FlightsController],
-  providers: [FlightsService],
+  providers: [
+    FlightsService,
+    {
+      provide: 'PG_POOL',
+      useFactory: () =>
+        new Pool({ connectionString: process.env.DATABASE_URL }),
+    },
+  ],
 })
 export class FlightsModule {}
