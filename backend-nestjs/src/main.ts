8d7e38691aaf54868241b0d9bc3b32ed38001e76
Danny Reina <dannyflatiron@gmail.com>
2025-11-02 19:53:42 -0500

    deps

--- backend-nestjs/src/main.ts
+++ backend-nestjs/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+      validationError: { target: false },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
   console.log('🚀 Server running at http://localhost:3000');
 }
